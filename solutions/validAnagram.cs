6ab875407d83c806ef2e1df8a330e420fd89dfef
esrabrown <essrcskn@hotmail.com>
2025-02-13 20:36:52 -0600

    2.problem

---
+++ solutions/validAnagram.cs
@@ -0,0 +1,26 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+
+*/
+
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+
+      if(s.Length != t.Length) return false;
+
+
+    }
+}
