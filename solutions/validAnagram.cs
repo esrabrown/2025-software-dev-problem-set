837bd755cdaadab8bb4a5a8be43feba496254e70
esrabrown <essrcskn@hotmail.com>
2025-02-13 20:37:45 -0600

    2.practice

--- solutions/validAnagram.cs
+++ solutions/validAnagram.cs
@@ -19,8 +19,17 @@ Output: false
 public class Solution {
     public bool IsAnagram(string s, string t) {
 
+     // If lengths are different they can't be anagrams.
       if(s.Length != t.Length) return false;
 
+     // Converting strings to character array
+     char[] char1 = s.ToCharArray();
+     char[] char2 = t.ToCharArray();
+
+     Array.Sort(char1);
+     Array.Sort(char2);
+
+     return new string(char1).Equals(new string(char2));
 
     }
 }
